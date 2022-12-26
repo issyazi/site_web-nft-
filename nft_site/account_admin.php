bd90e205ff042e42484d105a16d2599c6fbad9a6
candyliving <75974131+candyliving@users.noreply.github.com>
2022-12-27 00:13:47 +1000

    Add files via upload

--- nft_site/account_admin.php
+++ nft_site/account_admin.php
@@ -1,29 +1,30 @@
 <div> 
     <div class = "container">
         <div class = "purchased">
-            <p class = "status"> Purchased </p>
-            <div class = "bought">
-                <p>registered users</p>
-                <p>:</p>
-                <p>2</p>
-            </div>
-            <div class = "bought">
-                <p>new users for this week</p>
-                <p>:</p>
-                <p>2</p>
-            </div>
-            <div class = "bought">
+            <p class = "status"> Statistic </p>
+            <?php
+                include 'inc/connectproducts.php';
+                $all_users = mysqli_num_rows(mysqli_query($connectproducts, "SELECT * FROM `users`"));
+                $all_buys = mysqli_num_rows(mysqli_query($connectproducts, "SELECT * FROM `products` where `amount` = 0"));
+                echo'            
+                <div class = "bought">
+                    <p>registered users</p>
+                    <p>:</p>
+                    <p>'.$all_users.'</p>
+                </div>
+                <div class = "bought">
                 <p>purchased</p>
-                <p>:</p>
-                <p>8</p>
-            </div>
+                    <p>:</p>
+                    <p>'.$all_buys.'</p>
+                </div>
+                ';
+            ?>
         </div>
         <hr>
         <div class = "bt_admin">
             <a href="new_nft.php" class = "buttons"> new nft </a>
             <a href="edit_nft.php" class = "buttons"> change nft </a>
             <a href="delete_nft.php" class = "buttons"> delete nft </a>
-
         </div>
     </div>
 </div>
